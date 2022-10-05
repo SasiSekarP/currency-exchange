ecced135bc1d5fc49c935ab00e24e83743cc6885
SasiSekarP <109525298+SasiSekarP@users.noreply.github.com>
2022-10-05 20:41:31 +0530

    JS part is going on
    swap part is pending.

---
+++ script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const url = 'https://v6.exchangerate-api.com/v6/c5c8ba1aebae44331922bbd3/latest/';
+
+const containerEl = document.getElementById('container');
+
+function calculate() {
+    let currencyOneValue = document.getElementById('currencyOne').value;
+    let currencyTwoValue = document.getElementById('currencyTwo').value;
+    let currencyTwoAmountEl = document.getElementById('currencyTwoAmount');
+    let currencyOneAmountEl = document.getElementById('currencyOneAmount');
+
+
+    fetch(`${url}${currencyOneValue}`).then((response) => response.json()).then((data) => {
+        let rate = data.conversion_rates[currencyTwoValue];
+        console.log(data);
+        document.getElementById('rate').textContent = `1 ${currencyOneValue} = ${rate} ${currencyTwoValue}`;
+        document.getElementById('currencyTwoAmount').textContent = (rate * document.getElementById('currencyOneAmount').value);
+        currencyTwoAmountEl.value = (currencyOneAmountEl.value * rate);
+    });
+}
+
+calculate();
+
+containerEl.addEventListener('change', (e) => {
+    e.preventDefault();
+    calculate();
+});
